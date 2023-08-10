ecc73f0b3cf95dd596770f6ec76d8b94f83d0d78
Jacqueline <jgeyfen@gmail.com>
2023-08-09 21:27:10 -0700

    problem 4 complete

--- problems/04-get-full-name.js
+++ problems/04-get-full-name.js
@@ -10,9 +10,14 @@ let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
 getFullName(p2); // => 'Charlie Brown'
 ***********************************************************************/
 
+// key into the obj to get the firstname key and the lastname key
+// once we have those keys --> want the value of those specific keys
+// return values
+
 function getFullName(person) {
-  // Your code here
-}
+  return person.firstName + " " + person.lastName;
+ }
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = getFullName;
